c66ea67b4db8e64a371bb4c7cac7022fe79c7533
Alex Jordan <a.jordan6955@gmail.com>
2022-11-15 11:10:02 -0700

    Basic window

---
+++ mainwindow.h
@@ -0,0 +1,54 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QtCore>
+#include "btBulletDynamicsCommon.h"
+#include <Qt3DExtras/QTorusMesh>
+#include <Qt3DExtras/QSphereMesh>
+#include <Qt3DCore/qentity.h>
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+  Q_OBJECT
+
+public:
+  explicit MainWindow(QWidget *parent = 0);
+  ~MainWindow();
+
+
+private slots:
+    void on_actionStart_triggered();
+    void setup();
+
+private:
+  Ui::MainWindow *ui;
+  void timerEvent(QTimerEvent *);
+  void setTimeStep(double ts) {timeStep = ts;}
+
+
+  btBroadphaseInterface* broadphaseInterface;
+  btDefaultCollisionConfiguration* defaultCollisionConfig;
+  btCollisionDispatcher* collisionDispatcher;
+  btSequentialImpulseConstraintSolver* seqImpConstraintSolver;
+  btDiscreteDynamicsWorld* dynamicsWorld;
+
+
+//  Ground* mGround;
+//  std::vector<BouncyBall*> bouncyBalls;
+  double timeStep;
+
+  void initPhysics();
+  void createWorld();
+
+  Qt3DExtras::QSphereMesh *mSphere;
+  Qt3DCore::QEntity *mSphereEntity;
+  Qt3DCore::QEntity *mRootEntity;
+
+};
+
+#endif // MAINWINDOW_H
